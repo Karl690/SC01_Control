833099b61c4cc59b14d210a624f6bae790d67a5f
falcon198236 <falcon198236@outlook.com>
2025-11-27 16:09:05 +0100

    1.15 11/27/2025

--- main/RevisionHistory.h
+++ main/RevisionHistory.h
@@ -2,8 +2,8 @@
 
 #define SYSTEMINFO "SC01_Control"
 #define RevisionHeader ">Hy:L_Core:"
-#define MajorStep      "1.14"
-#define RevisionDate   "11/26/2025"
+#define MajorStep      "1.15"
+#define RevisionDate   "11/27/2025"
 #define SYSTEMVERSION  MajorStep " " RevisionDate
 #define CONNECTIONSTRING ">Hy:L_Core Rev " MajorStep ":" RevisionDate ":SC01_Control:---:\n"   //"1.000:0:0:0:USB"
 
